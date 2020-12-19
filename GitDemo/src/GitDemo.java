d47fb74dbb5685f050a1bfdfcb150ee40b75adf6
dell <dell@DESKTOP-8AA4PO4>
2020-12-19 13:55:45 +0530

    First Git Hub Commit

---
+++ GitDemo/src/GitDemo.java
@@ -0,0 +1,13 @@
+
+public class GitDemo {
+
+	public GitDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GitDemo");
+	}
+
+}
